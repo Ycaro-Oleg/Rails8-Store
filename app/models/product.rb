56025dfb65be7fbc7c1c59ed4b38414bb6acc9b7
Ycaro-Oleg <ycaro@coreplan.com.br>
2025-04-13 15:32:55 -0300

    Feat: Adding the views and controllers for handling the products , Basic CRUD structure

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ApplicationRecord
+  validates :name, presence: true
+
+end
+
+#Todos os registros da tabela products estao sendo interagidos a partir desse model product.rb dessa class Product
